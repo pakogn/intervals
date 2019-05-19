72521e5bd96708ca4a98c8da4998d7544de2bd7a
Francisco Daniel Garcia Noriega <pako.gn@hotmail.com>
2019-05-19 15:03:18 -0500

    feat: add flush interface to clear all the database.

--- intervals/index.php
+++ intervals/index.php
@@ -4,7 +4,7 @@ require __DIR__.'/../bootstrap/app.php';
 
 use App\Models\Interval;
 
-Interval::all();
+$intervals = Interval::all();
 
 ?>
 
@@ -26,21 +26,31 @@ Interval::all();
                 <h2>Intervals Manager</h2>
                 <p class="lead">A simple solution for managing intervals.</p>
             </div>
+            <?php if ($intervals->isNotEmpty()): ?>
+                <div class="row">
+                    <div class="col-md-8 offset-md-2 mb-5">
+                        <form>
+                            <br>
+                            <button class="btn btn-danger" type="submit"><i class="fa fa-sync"></i> Flush Database</button>
+                        </form>
+                    </div>
+                </div>
+            <?php endif?>
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <form>
                         <div class="row">
                             <div class="col-md-4">
-                                <label for="start_date">Start Date</label>
-                                <input type="date" class="form-control" id="start_date" required>
+                                <label for="date_start">Date Start</label>
+                                <input type="date" class="form-control" id="date_start" required>
                             </div>
                             <div class="col-md-4">
-                                <label for="end_date">End Date</label>
-                                <input type="date" class="form-control" id="end_date" required>
+                                <label for="date_end">Date End</label>
+                                <input type="date" class="form-control" id="date_end" required>
                             </div>
                             <div class="col-md-2">
-                                <label for="amount">Amount</label>
-                                <input type="text" class="form-control" id="amount" required>
+                                <label for="price">Price</label>
+                                <input type="text" class="form-control" id="price" required>
                             </div>
                             <div class="col-md-2">
                                 <br>
@@ -49,6 +59,8 @@ Interval::all();
                         </div>
                     </form>
                 </div>
+            </div>
+            <div class="row">
                 <div class="col-md-8 offset-md-2 mt-5">
                     <div class="table-responsive">
                         <table class="table table-striped table-hover table-bordered">
